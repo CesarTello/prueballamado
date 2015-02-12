d9c92542118eabdd9ad5f7f7181685460761d56c
CesarTello <cesartello@cableonda.net>
2015-02-12 13:33:51 -0500

    111111
    111111111

--- main.js
+++ main.js
@@ -1,3 +1,64 @@
+	window.addEventListener('load', function() {
+
+		var mybutton;
+		mybutton = document.getElementById('mybutton');
+
+		// Android 2.2 needs FastClick to be instantiated before the other listeners so that the stopImmediatePropagation hack can work.
+		FastClick.attach(mybutton);
+
+		//add event listener fx for controls
+		mybutton.addEventListener('touchend', function(event) {
+			//alert('touched on ok');
+		}, false);
+
+		mybutton.addEventListener('click', function(event) {
+			try
+			  {
+				  
+				  
+				  
+				  
+	navigator.startApp.check('com.waze', function(message) { /* success */
+		alert(message); // => OK
+	}, 
+	function(error) { /* error */
+		alert('error en startApp.check');
+	});
+	
+	
+	
+	
+	
+			  }
+			catch(err)
+			  {
+			  //Handle errors
+			  }			
+		}, false);
+
+		
+		
+		//after fastclick instantiation and listeners setup get rest of code in load fx
+		
+	  
+		  
+		  
+	}, false);
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
 	com.lampa.startapp
 	https://github.com/lampaa/com.lampa.startapp
