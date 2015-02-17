18e1bfe736e885200efdaf810b2b7e7f1b63c242
CesarTello <cesartello@cableonda.net>
2015-02-16 23:02:15 -0500

    222222
    2222222222

--- main.js
+++ main.js
@@ -18,7 +18,7 @@
 				  
 				  
 				  
-alert('dededededed');
+ window.plugins.launcher.canLaunch({packageName:'com.facebook.katana'}, successCallback, errorCallback);
 	
 	
 	
@@ -34,9 +34,13 @@ alert('dededededed');
 		
 		//after fastclick instantiation and listeners setup get rest of code in load fx
 		
-	  
+function successCallback(){
+	alert('exito');
+}
 		  
-		  
+function errorCallback(){
+	alert('fallo');
+}		  
 	}, false);
 	
 
@@ -53,46 +57,4 @@ alert('dededededed');
 
 
 
-/**
-	com.lampa.startapp
-	https://github.com/lampaa/com.lampa.startapp
-	
-	Phonegap 3 plugin for check or launch other application in android device (iOS support).
-	bug tracker: https://github.com/lampaa/org.apache.cordova.startapp/issues
-*/
-function init() {
-	/**
-	 * check the application is installed
-	 */
-	navigator.startApp.check('com.waze', function(message) { /* success */
-		alert(message); // => OK
-	}, 
-	function(error) { /* error */
-		alert('error en startApp.check');
-	});
-	
-	/**
-	 * start application without parameters
-	 */
-	navigator.startApp.start('com.waze', function(message) { /* success */
-		alert(message); // => OK
-	}, 
-	function(error) { /* error */
-		alert('error en startApp.start');
-	});
-	
-	/**
-	 * start application with parameters
-	 */
-	navigator.startApp.start([
-		'com.waze', // applucation
-		'com.teaway.teamenu.MainActivity', // activity
-		'product_id', // key
-		'102' // value
-	], function(message) { /* success */
-		alert(message); // => OK
-	}, 
-	function(error) { /* error */
-		alert('error en startApp.start con parametros');
-	});
-}
+
